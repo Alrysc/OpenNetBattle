f33ee8faa83647f811f9c662e212cabdee6c95d4
Alrysc <alrysc01@gmail.com>
2025-08-04 19:34:49 -0700

    Player.charged_time_table_func is called with charge level instead of Player

--- BattleNetwork/bindings/bnScriptedPlayer.cpp
+++ BattleNetwork/bindings/bnScriptedPlayer.cpp
@@ -189,7 +189,7 @@ frame_time_t ScriptedPlayer::CalculateChargeTime(const unsigned chargeLevel)
 {
   if (charge_time_table_func.valid())
   {
-    stx::result_t<frame_time_t> result = CallLuaCallbackExpectingValue<frame_time_t>(charge_time_table_func, weakWrap);
+    stx::result_t<frame_time_t> result = CallLuaCallbackExpectingValue<frame_time_t>(charge_time_table_func, chargeLevel);
 
     if (!result.is_error()) {
       return result.value();
