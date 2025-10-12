6062a223517b807b15fd4a8c04033f2abe117b27
Alrysc <alrysc01@gmail.com>
2025-10-12 15:20:57 -0700

    Use this->pivotPred

--- BattleNetwork/bnFolderEditScene.h
+++ BattleNetwork/bnFolderEditScene.h
@@ -207,8 +207,8 @@ private:
         this->lastIndex = index;
       }
 
-      if (pivotPred) {
-        auto pivot = std::partition(this->container.begin(), this->container.end(), pivotPred);
+      if (this->pivotPred) {
+        auto pivot = std::partition(this->container.begin(), this->container.end(), this->pivotPred);
         size_t pivotDist = std::distance(this->container.begin(), pivot);
 
         std::vector<T> copy = std::vector<T>(this->container.begin(), pivot);
